d9a73ec5678f3c96e5ebb35f916bb53fc859bc29
Diilu Behera <dushmanta0511@gmail.com>
2022-12-08 22:03:57 +0530

    new component imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box>
+      <Navbar className='navbar'/>
+      <MainContent />
+      <Footer />
+    </Box>
   )
 }
 
