b1b361229717765b369e010d8eeff871310910f4
Diilu Behera <dushmanta0511@gmail.com>
2022-12-09 09:51:21 +0530

    resizing the progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,8 +1,22 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
-const ProgressBar = () => {
+const ProgressBar = ({done}) => {
+    const [style, setStyle] = React.useState({});
+	
+	setTimeout(() => {
+		const newStyle = {
+			opacity: 1,
+			width: `${done}%`
+		}
+		
+		setStyle(newStyle);
+	}, 100);
   return (
-    <div>ProgressBar</div>
+    <Box className="progress">
+		<Box className="progress-done" style={style}>
+		</Box>
+	</Box>
   )
 }
 
