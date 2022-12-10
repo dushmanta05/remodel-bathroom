1449678c6ded9d246e5e5465145442d59d660491
Diilu Behera <dushmanta0511@gmail.com>
2022-12-11 00:03:45 +0530

    multi-page form component added & rendered

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
-import ProgressBar from './ProgressBar'
-import UserInput from './UserInput'
-const Form = () => {
-    state = {
-        step: 1
-    }
+import React, {useState} from 'react'
+import { Box, Stack, Typography } from '@mui/material'
 
-    prevStep = () => {
-        const { step } = this.state;
-        this.setState({ step: step - 1 });
-    }
-    nextStep = () => {
-        const { step } = this.state;
-        this.setState({ step: step + 1 });
-    }
-    handleChange = input => e => {
-        this.setState({ [input]: e.target.value });
+import ProgressBar from './ProgressBar'
+import SelectService from './SelectService'
+import Address from './Address'
+import PersonalInfo from './PersonalInfo'
+
+const Form = () => {
+    const [step, setStep] = useState(0)
+    const [done, setDone] = useState(10)
+
+    const Display = () => {
+        if(step === 0){
+            return <SelectService setStep={setStep} setDone={setDone}/>
+        } else if (step === 1){
+            return <Address setStep={setStep} setDone={setDone}/>
+        } else {
+            return <PersonalInfo setStep={setStep} setDone={setDone}/>
+        }
     }
 
   return (
-    <Box>
-        <Typography>Get A Free Quote</Typography>
-        <ProgressBar />
-        <UserInput />
+    <Box bgcolor='#fff' width='600px' borderRadius='5px' >
+        <Typography variant='h4' textAlign='center' mt='30px' fontWeight='bold'>Get A Free Quote</Typography>
+        <Stack mt='25px' ml='30px' mr='30px' direction='row' justifyContent='space-between'>
+            <Typography variant='body2' fontSize='15px' fontWeight='bold'>
+                Step {step + 1} of 4
+            </Typography>
+            <Typography variant='body2' fontSize='15px' fontWeight='bold'>{done}%</Typography>
+    </Stack>
+        <ProgressBar done={done}/>
+        <Box>{Display()}</Box>
     </Box>
   )
 }
