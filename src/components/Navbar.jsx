cfa8fd5ebefcb89b6cbaac230698e892cd68987a
Diilu Behera <dushmanta0511@gmail.com>
2022-12-11 00:06:05 +0530

    style update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import { Box, Button, IconButton, Stack, Typography } from '@mui/material'
 
-import Logo from '/src/Images/bathroom.png'
-import CallIcon from '/src/Images/phonecall.png'
+import Logo from '/src/assets/Images/bathroom.png'
+import CallIcon from '/src/assets/Images/phonecall.png'
 
 const Navbar = () => {
   return (
-    <Stack direction='row' justifyContent='space-between' alignItems='center' m='30px 50px' >
-      <Stack direction='row' alignItems='center' justifyContent='flex-start' spacing={2}>
-        <img src={Logo} alt='logo' style={{width: '48px'}} />
-        <Stack direction='column'>
-          <Typography component='h2' fontWeight='700'><span style={{color: '#FF7000'}}>HOUSTON</span> BATHROOM REMODELING</Typography>
-          <Typography color={{color: '#c0bec0'}}>1-Day Bath Remodels</Typography>
+    <Box className='navbar'>
+      <Stack direction='row' justifyContent='space-between' alignItems='center' mt='15px' ml='50px' mr='50px' mb='15px' >
+        <Stack direction='row' alignItems='center' justifyContent='center' spacing={2}>
+          <img src={Logo} alt='logo' style={{width: '48px'}} />
+          <Stack direction='column'>
+            <Typography component='h2' fontFamily='Arial' fontWeight='bold'><span style={{color: '#FF7000'}}>HOUSTON</span> BATHROOM REMODELING</Typography>
+            <Typography color={{color: '#c0bec0'}}>1-Day Bath Remodels</Typography>
+          </Stack>
         </Stack>
-      </Stack>
       <Stack>
-        <Button borderRadius='4px' sx={{color: '#fff', bgcolor: '#FF7000', height: '50px', width: '200px'}}>
-            <IconButton sx={{bgcolor: '#FFF', height: '45px', width: '45px', borderRadius: '4px', pl: '0'}}>
+        <Button sx={{color: '#fff', bgcolor: '#FF7000', height: '50px', width: '200px'}}>
+          <IconButton size='large' sx={{bgcolor: '#FFF', borderRadius: '4px', position: 'absolute', left: '2.5px'}}>
             <img src={CallIcon} style={{height: '20px', width: '20px'}}/>
-            </IconButton>
-            <Typography variant='h6'>(855) 501-4469</Typography>
+          </IconButton>
+          <Typography sx ={{ml: '40px'}}variant='h6'>(855) 501-4469</Typography>
         </Button>
       </Stack>
     </Stack>
+    </Box>
   )
 }
 
